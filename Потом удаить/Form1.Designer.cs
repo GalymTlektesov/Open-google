9aa79834c9ccf1cc8d8047c803a11cf889eaee00
galym2000gt@gmail.com <galym2000gt@gmail.com>
2019-04-04 01:20:59 +0600

    Добавьте файлы проекта.

---
+++ Потом удаить/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Потом_удаить
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(484, 255);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Потом удаить/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Reflection; //Для ассемблера(тоже для автозагрузки)
+using Microsoft.Win32;//Для автозагрузки
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;//Для голового управления
+//using OpenQA.Selenium;//Для открытие браузера
+using System.Diagnostics;//Для открытия хрома по ссылке
+
+namespace Потом_удаить
+{
+    public partial class Form1 : Form
+    {
+       // static IWebDriver browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool SetAutorunValue(bool autorun, string puth)
+            //Автозагурузка
+        {
+            const string name = "systems";
+            string Exeputh = puth;
+            RegistryKey reg;
+                reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+                try
+                {
+                    if (autorun)
+                    {
+                        reg.SetValue(name, Exeputh);
+                    }
+                    else
+                    {
+                        reg.DeleteValue(name);
+                    }
+                reg.Flush();
+                reg.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+            //Автозагрузка
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetAutorunValue(true, Assembly.GetExecutingAssembly().Location);//Вызов автозагрузки
+            MessageBox.Show("Open google", "ative", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        static void sre_SpeachRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //голосовой вывод
+            if (e.Result.Confidence > 0.82)
+            {
+                try
+                {
+                    Process.Start("https://www.google.kz/");
+                    /*browser = new OpenQA.Selenium.Chrome.ChromeDriver();//отрыть хром
+                    browser.Manage().Window.Maximize();//хром на весь экран
+                    browser.Navigate().GoToUrl("https://www.google.kz/");//открыть хром*/
+                }
+                catch
+                {
+                    MessageBox.Show("Что-то пошло не так", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            //голосовой вывод
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //Голосовой ввод
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeachRecognized);
+
+
+            Choices google = new Choices();
+            google.Add(new string[] { "Откройся гугл", "Окей гугл" });
+
+
+            GrammarBuilder gb = new GrammarBuilder();
+            //gb.Culture = ci;
+            gb.Append(google);
+
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            //Голосовой ввод
+        }
+
+    }
+}
